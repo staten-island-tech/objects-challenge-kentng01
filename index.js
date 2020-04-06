e37b861ebb6b4571736f216adba3866ba6d24c70
kentng01 <57498837+kentng01@users.noreply.github.com>
2020-04-06 18:34:09 -0400

    asfasfas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 const Caroline = {
     name: "Caroline",
     dob: 2004,
@@ -7,3 +9,37 @@ const Caroline = {
         return 2020 - this.dob;
     }
 };
+
+const Homor = {
+    name: "Homer",
+    age: 32,
+    family: {
+        wife:{
+            name: "Marge"
+        },
+        son: {
+            name: "Bart"
+        }
+    },
+    pets: ["cat", "dog"]
+};
+
+*/
+
+const Haoran = {
+    name: "how",
+    dob: 2001,
+    year: "Senior",
+    age: function() {
+        return 2020 - this.dob
+    }
+};
+
+/*const Eric = Haoran.year; // reference to Haoran
+const Christian = {};
+Christian.dob = Haoran.dob;
+Christian.dob = 2003;*/
+const calculator = {
+    add: //function x + y,
+    subtract: //function x - y
+}
