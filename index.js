2bb3a5b291dfd17d89b7e7de5b7a8c9c2a7b3240
kentng01 <57498837+kentng01@users.noreply.github.com>
2020-04-06 18:50:34 -0400

    i actually did it woooo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const Homor = {
     pets: ["cat", "dog"]
 };
 
-*/
+
 
 const Haoran = {
     name: "how",
@@ -35,11 +35,105 @@ const Haoran = {
     }
 };
 
-/*const Eric = Haoran.year; // reference to Haoran
+const Eric = Haoran.year; // reference to Haoran
 const Christian = {};
 Christian.dob = Haoran.dob;
-Christian.dob = 2003;*/
+Christian.dob = 2003;
 const calculator = {
     add: //function x + y,
     subtract: //function x - y
 }
+*/
+
+//Challenge Questions
+  //1) Filter all presidents, leaving only the Democratic ones
+   
+  //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+   
+  //3) return only the last three presidents
+   
+  //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+   
+  //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+   
+  
+/////challenge 1
+const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+];
+
+const democraticPresidents =  presidents.filter(function(president) {
+	return president.party == “Democratic”;
+});
+
+/////challenge 2
+
+let Trump = Trump;
+if (trump === R){
+    console.log("Reublican");
+}
+
+/////challenge 3
+const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+];
+
+const lastthreePresidents =  presidents.filter(function(president) {
+	return president.name == “Trump, Obama, Bush”;
+});
+
+/////challenge 4
+
+const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+];
+
+let demooratic2President = [
+    {
+      president: 'Obama',
+      party: Democratic,
+    },
+    {
+        president: 'Clinton',
+        party: Democratic,
+    },
+    {
+        president: 'Johnson',
+        party: Democratic,
+    },
+
+    console.log("Democratic");
+  ]
